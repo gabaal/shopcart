0fc4690fa901b4fc506fe12a15658a6d69ba8a2f
Gerald <geraldbaalham@live.co.uk>
2025-04-02 13:09:44 +0100

    Implement header components and layout; add shopping cart and favorite button functionality

---
+++ components/SearchBar.tsx
@@ -0,0 +1,10 @@
+import { SearchIcon } from "lucide-react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <SearchIcon className="w-5 h-5 hover:text-shop_light_green hoverEffect" />
+    </div>
+  );
+};
+export default SearchBar;
---
+++ components/SignIn.tsx
@@ -0,0 +1,8 @@
+const SignIn = () => {
+  return (
+    <button className="text-sm font-semibold text-lightColor hover:text-darkColor hover:cursor-pointer hoverEffect">
+      Login
+    </button>
+  );
+};
+export default SignIn;
